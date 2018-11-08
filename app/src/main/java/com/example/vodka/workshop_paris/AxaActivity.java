cfd60bd9ed7ff5634283b5d168e61f621fc8ca0d
saber <saberfeki5@gmail.com>
2018-11-08 01:21:03 +0100

    first commit

---
+++ app/src/main/java/com/example/vodka/workshop_paris/AxaActivity.java
@@ -0,0 +1,13 @@
+package com.example.vodka.workshop_paris;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AxaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_axa);
+    }
+}
---
+++ app/src/main/java/com/example/vodka/workshop_paris/SubscribActivity.java
@@ -0,0 +1,13 @@
+package com.example.vodka.workshop_paris;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubscribActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscrib);
+    }
+}
